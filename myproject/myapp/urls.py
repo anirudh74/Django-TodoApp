8271d1f6e90f5f918cc2b76934ee3b9ff14b8e7c
anirudh74 <anirudhkonduri@anirudhkondurithoughtworkscom.local>
2024-05-20 16:15:16 +0530

    Added code for api

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
    path('add/', views.add, name='add'),
    path('complete/<int:id>', views.complete, name='complete'),
    path('delete/<int:id>', views.delete, name='delete'),
+   path('api/listallTodoItems',views.listallTodoItems.as_view(),name='listallTodoItems'),
+   path('api/api_add_todo',views.api_add_todo,name='api_add_todo'),
+   path('api/task-change-status/<str:pk>/', views.change_status, name='change-status'),  # New endpoint
+   path('api/todos/<int:pk>/', views.delete_todo, name='delete-todo'),
+
 ]
